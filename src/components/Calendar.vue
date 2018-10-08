5ae7a03355adf46bf06c8a4d2a985dd13846e9e3
Robin Pedersen <robin@smartdok.com>
2018-10-08 12:14:44 +0200

    Calendar component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -5,12 +5,127 @@ import moment from 'moment';
 export default Vue.extend({
   name: 'calendar',
 
+  props: {
+    date: {
+      type: Object as () => moment.Moment,
+      default: () => moment(),
+    },
+  },
+
   methods: {
+    renderHeader(): VNode[] {
+      const h = this.$createElement;
+      let weekdays = moment.weekdays();
+      let start = moment(this.date).startOf('isoWeek');
+      let end = moment(this.date).endOf('isoWeek');
+
+      let children: VNode[] = [];
+      for (let d = start; d < end; d = d.add(1, 'day')) {
+        children.push(h('span', {
+            class: 'weekday',
+          },
+          weekdays[d.day()]));
+      }
+
+      return children;
+    },
+
+    renderMonth(): VNode[] {
+      let start = moment(this.date).startOf('month').startOf('isoWeek');
+      let end = moment(this.date).endOf('month').endOf('isoWeek');
+
+      let children: VNode[] = [];
+      for (let d = start; d < end; d = d.add(1, 'day')) {
+        children.push(this.renderCell(d));
+      }
+
+      return children;
+    },
+
+    renderCell(date: moment.Moment) {
+      const h = this.$createElement;
+
+      const d = date.format('D');
+      const text = date.format('LLLL');
+      return h('span', {
+        class: {
+          'day': true,
+          'weekend': date.day() === 6 || date.day() === 0,
+          'other-month': date.month() !== this.date.month(),
+        },
+      },
+      [
+        h('span', {
+          class: {
+            'date': true,
+            'sunday': date.day() === 0,
+          },
+        }, d),
+        // h('span', {class: 'text'}, text),
+      ]);
+    },
   },
 
   render(): VNode {
     const h = this.$createElement;
-    return h('div', 'clendar');
+    return h('div', {class: 'calendar'}, [
+      this.renderHeader(),
+      this.renderMonth(),
+    ]);
   },
 });
 </script>
+
+<style lang="scss" scoped>
+
+$inner-border-width: 1px;
+$inner-border: $inner-border-width solid #ccc;
+
+.calendar {
+  display: grid;
+
+  border-radius: 3px;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: auto;
+  grid-auto-rows: 100px;
+
+  > span {
+    padding: 8px;
+
+    border: $inner-border;
+    margin-right: -$inner-border-width;
+    margin-bottom: -$inner-border-width;
+  }
+}
+
+.weekday {
+  background-color: #efefef;
+}
+
+.date {
+  float: right;
+  font-size: .9em;
+  // font-weight: bold;
+}
+
+.text {
+  font-size: .7em;
+}
+
+.day {
+  background-color: #fff;
+}
+
+.weekend .date {
+  // background-color: #efefef;
+}
+
+.sunday {
+  color: red;
+}
+
+.other-month {
+  background-color: #efefef;
+}
+</style>
+
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,20 +1,148 @@
 <template>
   <div>
     <h2>Calendar demo</h2>
+    <div class="options">
+      <button type="button" @click="onPrevClick">&lt;&lt;</button>
+      <button type="button" @click="onTodayClick">{{ today }}</button>
+      <button type="button" @click="onNextClick">&gt;&gt;</button>
+    </div>
 
-    <calendar />
+    <div>
+      <transition :name="transition">
+        <div class="month" :key="month">
+          <calendar :date="date" />
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
 
 <script lang="ts">
 import Vue from 'vue';
+import moment from 'moment';
 import Calendar from '@/components/Calendar.vue';
 
 export default Vue.extend({
   components: {
     Calendar,
   },
-})
+
+  data() {
+    let today = moment().startOf('day');
+    return {
+      date: today,
+      today: today.format('LL'),
+      transition: 'slide-left',
+    };
+  },
+
+  computed: {
+    month(): number {
+      return this.date.month();
+    },
+  },
+
+  methods: {
+    onPrevClick() {
+      this.transition = 'slide-right';
+      this.date = moment(this.date).subtract(1, 'month');
+    },
+
+    onNextClick() {
+      this.transition = 'slide-left';
+      this.date = moment(this.date).add(1, 'month');
+    },
+
+    onTodayClick() {
+      let today = moment().startOf('day');
+      if (today < this.date) {
+        this.transition = 'slide-right';
+      } else {
+        this.transition = 'slide-left';
+      }
+      this.date = today;
+    },
+  },
+});
 </script>
 
+
+<style lang="scss" scoped>
+.options {
+  margin-bottom: 20px;
+  text-align: center;
+
+  button {
+    padding: 5px 10px;
+    margin: 0 10px;
+  }
+}
+
+.month {
+  position: absolute;
+  width: calc(100vw - 120px);
+}
+
+.slide-left-enter-active {
+  animation: slide-left-in .5s;
+  opacity: 0;
+}
+
+.slide-left-leave-active {
+  animation: slide-left-out .5s;
+}
+
+@keyframes slide-left-in {
+  from {
+    transform: translateX(150px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-left-out {
+  from {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(-150px);
+    opacity: 0;
+  }
+}
+
+.slide-right-enter-active {
+  animation: slide-right-in .5s;
+  opacity: 0;
+}
+
+.slide-right-leave-active {
+  animation: slide-right-out .5s;
+}
+
+@keyframes slide-right-in {
+  from {
+    transform: translateX(-150px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-right-out {
+  from {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(150px);
+    opacity: 0;
+  }
+}
+</style>
