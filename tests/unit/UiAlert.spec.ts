4b1595d808b182993be91713ebc56e5c532453a3
Robin Pedersen <robin@smartdok.com>
2018-11-19 10:39:07 +0100

    Initialize unit/snapshot testing

---
+++ tests/unit/UiAlert.spec.ts
@@ -0,0 +1,34 @@
+import { mount } from '@vue/test-utils';
+import UiAlert from '@/components/UiAlert.vue';
+
+describe('UiAlert', () => {
+  it('renders successful alert', () => {
+    const wrapper = mount(UiAlert, {
+      propsData: { success: true },
+      slots: {
+        default: 'Success message',
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders warning alert', () => {
+    const wrapper = mount(UiAlert, {
+      propsData: { warning: true },
+      slots: {
+        default: 'Warning message',
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders error alert', () => {
+    const wrapper = mount(UiAlert, {
+      propsData: { error: true },
+      slots: {
+        default: 'Error message',
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
