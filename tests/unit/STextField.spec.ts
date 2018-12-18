189db5569d05a5c176117ab26172205fb4bf943b
Robin Pedersen <robin@smartdok.com>
2018-12-18 14:55:20 +0100

    Tests

---
+++ tests/unit/STextField.spec.ts
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils';
+import STextField from '@/components/STextField.vue';
+
+describe('STextField', () => {
+  it('renders text field', () => {
+    const wrapper = mount(STextField, {
+      propsData: {
+        label: 'Text field',
+        value: 'foobar',
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('emits entered value', () => {
+    const wrapper = mount(STextField, {
+      propsData: {
+        label: 'Text field',
+        value: '',
+      },
+    });
+    let input = wrapper.findAll('input');
+    input.setValue('kokobaba');
+    input.trigger('change');
+
+    expect(wrapper.emitted().input[0]).toEqual(['kokobaba']);
+  });
+});
