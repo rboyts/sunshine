46e2e5f0b6fe5c1dd35c601ca2b47627f1d89906
Robin Pedersen <robin@smartdok.com>
2019-01-03 14:05:42 +0100

    Fix `classes` warning

--- src/components/SRadioButton.vue
+++ src/components/SRadioButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <label :class="classes">
-    <span class="s-radio-button__box">
+  <label :class="classes()">
+    <span :class="classes('box')">
       <input
         type="radio"
         :name="name"
@@ -20,9 +20,6 @@
 
 <script>
 import Vue from 'vue';
-import { classHelper } from '../lib/utils';
-
-const radioButtonClassHelper = classHelper('s-radio-button');
 
 export default Vue.extend({
   name: 's-radio-button',
@@ -51,11 +48,11 @@ export default Vue.extend({
   },
 
   computed: {
-    classes() {
-      return radioButtonClassHelper({
+    modifiers() {
+      return {
         focus: this.hasFocus,
         inactive: this.inactive,
-      });
+      };
     },
   },
 
