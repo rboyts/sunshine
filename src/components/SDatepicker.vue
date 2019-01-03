6cd19d62b51ad9d6de23742db6c1e1e43eed000a
Thomas Nielsen <thomasnielsen@Thomass-MacBook-Pro-2.local>
2019-01-03 11:11:35 +0100

    Create untracked local dev-env, add Datepicker.vue

---
+++ src/components/SDatepicker.vue
@@ -0,0 +1,13 @@
+<template>
+    <div :class="classes()">
+        <h2>Datepicker</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 's-datepicker',
+});
+
+</script>
