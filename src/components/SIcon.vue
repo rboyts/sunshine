72cbf71816407efc488bb3f422eb9b8033b33094
Robin Pedersen <robin@smartdok.com>
2019-01-11 08:59:13 +0100

    Use font-awesome icons in s-icon component (for now)

--- src/components/SIcon.vue
+++ src/components/SIcon.vue
@@ -1,12 +1,14 @@
 <template>
-  <span :class="classes()">{{ icon }}</span>
+  <span :class="classes()"><i :class="iconClasses" /></span>
 </template>
 
 
 <script lang="ts">
 import Vue from 'vue';
+import mixins from 'vue-typed-mixins';
+import { ClassesMixin } from '../lib/utils';
 
-export default Vue.extend({
+export default mixins(ClassesMixin).extend({
   name: 's-icon',
 
   props: {
@@ -17,13 +19,11 @@ export default Vue.extend({
   },
 
   computed: {
-    icon() {
-      switch (this.name) {
-        case 'hourglass':
-          return String.fromCharCode(0x231B);
-        default:
-          return String.fromCharCode(0x2318);
-      }
+    iconClasses(): object {
+      return {
+        far: true,
+        [`fa-${this.name}`]: true,
+      };
     },
   },
 });
