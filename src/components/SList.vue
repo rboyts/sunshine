4a1772f7a25ee7a996689ba408c5b3390ae5cc41
Robin Pedersen <robin@smartdok.com>
2019-01-21 09:33:54 +0100

    Auto scrolling in dropdown lists
    TODO: Click handling is broken

--- src/components/SList.vue
+++ src/components/SList.vue
@@ -11,5 +11,29 @@ import { ClassesMixin } from '../lib/utils';
 
 export default mixins(ClassesMixin).extend({
   name: 's-list',
+
+  methods: {
+    ensureSelectedVisible() {
+      const el = this.$el as HTMLElement;
+      const selected = this.$el.querySelector('.s-list-item--selected') as HTMLElement;
+      if (!selected) return;
+
+      if (selected.offsetTop < this.$el.scrollTop) {
+        selected.scrollIntoView(true);
+      } else if (selected.offsetTop + selected.offsetHeight > el.scrollTop + el.offsetHeight) {
+        selected.scrollIntoView(false);
+      }
+    },
+  },
+
+  updated() {
+    this.ensureSelectedVisible();
+  },
+
+  mounted() {
+    Vue.nextTick(() => {
+      this.ensureSelectedVisible();
+    });
+  },
 });
 </script>
