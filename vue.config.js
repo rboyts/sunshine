5eedb6eb6c81ade8c45626b685f68e91473f7642
Robin Pedersen <robin@smartdok.com>
2019-02-18 12:21:38 +0100

    Externalize dependencies
    This makes the library build a lot smaller.
    Also adds bundle analyzer plugin.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   chainWebpack: config => {
     config.module.rules.delete('svg');
+    config.externals({
+      moment: 'moment',
+      d3: 'd3',
+    });
+  },
+
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false,
+    },
   },
 };
