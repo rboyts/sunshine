f1711017d9ce52056ecf4657e5af938de2392de4
Robin Pedersen <robin@smartdok.com>
2019-02-20 16:11:44 +0100

    Remove unnecessary nesting

--- src/components/SDataTable.vue
+++ src/components/SDataTable.vue
@@ -1,54 +1,51 @@
 <template>
-  <div class="flex-column flex-grow">
-    <data-table-internal
-      class="flex-grow"
-      :items="items"
-      :total="total"
-      :skip="skip"
-      :sorting="sorting"
-      :columns="visibleColumns"
-      @sort="onSort"
-      @visible-rows="onVisibleRows"
-      @move-column="onMoveColumn"
-      @open-item="onOpenItem"
+  <data-table-internal
+    class="flex-grow"
+    :items="items"
+    :total="total"
+    :skip="skip"
+    :sorting="sorting"
+    :columns="visibleColumns"
+    @sort="onSort"
+    @visible-rows="onVisibleRows"
+    @move-column="onMoveColumn"
+    @open-item="onOpenItem"
 
-      v-bind="$attrs"
-    >
+    v-bind="$attrs"
+  >
 
-      <template slot="menu">
-        <s-menu v-model="menuOpen">
-          <div slot="activator" :class="classes('more', {open: menuOpen})">
-            <s-icon package="sunshine24" name="more" />
-          </div>
+    <template slot="menu">
+      <s-menu v-model="menuOpen">
+        <div slot="activator" :class="classes('more', {open: menuOpen})">
+          <s-icon package="sunshine24" name="more" />
+        </div>
 
-          <template slot="content">
-            <s-list>
-              <s-list-item
-                v-for="(oc, i) in orderedColumns"
-                :key="oc.column.key"
-                checkable
-                :inactive="i === 0"
-                :checked="oc.visible"
-                @change="onToggleColumn(i, $event)"
-              >
-                {{ oc.column.title }}
-              </s-list-item>
-            </s-list>
-          </template>
-        </s-menu>
-      </template>
+        <template slot="content">
+          <s-list>
+            <s-list-item
+              v-for="(oc, i) in orderedColumns"
+              :key="oc.column.key"
+              checkable
+              :inactive="i === 0"
+              :checked="oc.visible"
+              @change="onToggleColumn(i, $event)"
+            >
+              {{ oc.column.title }}
+            </s-list-item>
+          </s-list>
+        </template>
+      </s-menu>
+    </template>
 
-      <!-- Pass on all named slots -->
-      <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot"/>
+    <!-- Pass on all named slots -->
+    <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot"/>
 
-      <!-- Pass on all scoped slots -->
-      <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope">
-        <slot :name="slot" v-bind="scope"/>
-      </template>
+    <!-- Pass on all scoped slots -->
+    <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope">
+      <slot :name="slot" v-bind="scope"/>
+    </template>
 
-    </data-table-internal>
-
-  </div>
+  </data-table-internal>
 </template>
 
 <script lang="ts">
