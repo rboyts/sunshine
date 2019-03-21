7ccee4babf113efd45a0d3177502473b03f53f33
Thomas Nielsen <tsn1985@gmail.com>
2019-03-21 09:57:35 +0100

    Change computed property from and to dates

--- src/components/SDatePicker/SDateRange.vue
+++ src/components/SDatePicker/SDateRange.vue
@@ -4,13 +4,13 @@
       <template v-slot:activator>
         <div class="s-date-field-range-input" @click="toggleOpen">
           <s-date-to-stringinput
-            v-model="internalValue.from"
+            v-model="from"
             :locale="locale"
             :label="fromLabel"
             :format="format"
           />
           <s-date-to-stringinput
-            v-model="internalValue.to"
+            v-model="to"
             :locale="locale"
             :label="toLabel"
             :format="format"
@@ -20,6 +20,7 @@
       <template v-slot:content>
         <s-datepicker
           rangeSelect
+          :includeMenu="menu"
           :locale="locale"
           :format="format"
           v-model="internalValue"
@@ -34,6 +35,7 @@ import moment, { Moment } from 'moment';
 import SMenu from '../SMenu.vue';
 import SDatepicker from './SDatepicker.vue';
 import SDateToStringinput from './SDateToStringinput.vue';
+import { ICalendarPeriod } from '../types';
 
 export default Vue.extend({
   name: 's-date-range',
@@ -47,20 +49,44 @@ export default Vue.extend({
   data() {
     return {
       isOpen: false,
-      internalValue: this.value,
+      internalValue: this.value as ICalendarPeriod,
       locale: moment.locale(),
       format: moment.localeData().longDateFormat('L'),
     };
   },
 
   props: {
+    includeMenu: {
+      type: Boolean,
+      default: false,
+    },
     value: {} as any,
     fromLabel: String,
     toLabel: String,
   },
 
   computed: {
-
+    from: {
+      set(newValue: Moment) {
+        this.internalValue.from = newValue;
+      },
+      get(): Moment {
+        if (!this.internalValue) return moment();
+        return this.internalValue.from;
+      },
+    },
+    to: {
+      set(newValue: Moment) {
+        this.internalValue.to = newValue;
+      },
+      get(): Moment {
+        if (!this.internalValue) return moment();
+        return this.internalValue.to;
+      },
+    },
+    menu(): Boolean {
+      return this.includeMenu;
+    },
   },
 
   watch: {
