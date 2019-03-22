3581a3c1ec6683786ade511ea2d8e5daf6aeaef5
Tage Guddingsmo <tage@visma.com>
2019-03-22 16:25:03 +0100

    Added counter label

--- src/components/SBaseInput.vue
+++ src/components/SBaseInput.vue
@@ -1,6 +1,7 @@
 <template>
   <div :class="inputClass">
     <span v-if="label" :class="labelClass">{{ label }}</span>
+    <span v-if="maxchar > 0" :class="counterClass">{{ count }}/{{ maxchar }}</span>
     <slot />
   </div>
 </template>
@@ -11,6 +12,7 @@ import { classHelper } from '../lib/utils';
 
 const inputClassHelper = classHelper('s-input');
 const labelClassHelper = classHelper('s-input', 'label');
+const counterClassHelper = classHelper('s-input', 'counter');
 
 export default Vue.extend({
   name: 's-base-input',
@@ -29,6 +31,14 @@ export default Vue.extend({
       type: Boolean,
       default: false,
     },
+    maxchar: {
+      type: Number,
+      default: 0,
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
 
     // If readonly, the label is shown full size even when the input has focus,
     // when empty
@@ -39,6 +49,7 @@ export default Vue.extend({
   },
 
   computed: {
+
     inputClass(): object {
       return inputClassHelper({
         focus: this.hasFocus,
@@ -51,7 +62,20 @@ export default Vue.extend({
         aside: (this.hasFocus && !this.readonly) || !this.isEmpty,
       });
     },
+    counterClass(): object {
+      const usedField = this.count / this.maxchar;
+      let show = false;
+      let stop = false;
+      if (usedField >= 1) {
+        stop = true;
+      } else if (usedField >= 0.8) {
+        show = true;
+      }
+      return counterClassHelper({
+        show,
+        stop,
+      });
+    },
   },
-
 });
 </script>
