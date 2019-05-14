14083ec0404cae83248b3d0fdf0a8f99fe74a543
Robin Pedersen <robin@smartdok.com>
2019-05-14 23:16:03 +0200

    Refactor SDatepickerMonth component
    - Use $class helper
    - Rename some methods

--- src/components/SDatepicker/SDatepickerMonth.vue
+++ src/components/SDatepicker/SDatepickerMonth.vue
@@ -1,25 +1,24 @@
 <template>
-  <span class="s-datepicker__grid">
-    <h3 class="s-datepicker__month">{{ translateMonthName(month.month, month.year) }}</h3>
-    <div class="s-datepicker__weeks">
+  <span :class="$class('grid')">
+    <h3 :class="$class('month')">{{ translateMonthName(month.month, month.year) }}</h3>
+    <div :class="$class('weeks')">
       <span
-        class="s-datepicker__weeks__week"
+        :class="$class('weeks', 'week')"
         v-for="w in month.weeksInMonth"
         :key="w"
       >{{ w }}</span>
     </div>
     <span
-      class="s-datepicker__date--overlapping"
+      :class="$class('date', { overlapping: true })"
       v-for="(x, k) in month.previousMonthDays"
       :key="`${x}-${month.month}-${k}`"
     >{{ x }}</span>
     <span
-      class="s-datepicker__date"
       v-for="a in month.daysInMonth"
       :key="`${a}-${month.month}`"
-      :class="setDateClasses(month.month, a, month.year)"
+      :class="getDateClasses(month.month, a, month.year)"
       @click="mouseClick({ y: month.year, M: month.month, d: a })"
-      @mousedown="mousedown({ y: month.year, M: month.month, d: a }, $event)"
+      @mousedown="onMouseDown({ y: month.year, M: month.month, d: a }, $event)"
       @mouseup="$emit('mouse-drag-end', { y: month.year, M: month.month, d: a })"
       @mouseover="mouseOverEvent({ y: month.year, M: month.month, d: a })"
     >
@@ -28,7 +27,7 @@
       </span>
     </span>
     <span
-      class="s-datepicker__date--overlapping"
+      :class="$class('date', { overlapping: true })"
       v-for="(x, k) in (6 - month.lastDay)"
       :key="`${x}-${month.month}-${k}`"
     >{{ x }}</span>
@@ -40,12 +39,11 @@ import Vue from 'vue';
 import moment, { Moment } from 'moment';
 import { IMonth, ICalendarPeriod, IMomentPayload } from '../types';
 
-/**
- * Obs: for comparison of dates this uses default date format (ISO 8601) 'YYYY-MM-DD'
- */
-
 export default Vue.extend({
   name: 'SDatepickerMonth',
+
+  // CSS class names start with 's-datepicker' instead of 's-datepicker__month'
+  $_className: 's-datepicker',
 
   props: {
     range: {
@@ -88,30 +86,16 @@ export default Vue.extend({
   },
 
   methods: {
-    setDateClasses(m: number, d: number, y: number) {
-      let classes = [];
-      if (this.range && this.fromDate && this.toDate) {
-        if (this.isInPeriod(m, d, y, this.fromDate, this.toDate)) {
-          classes.push('s-datepicker__date--between');
-        }
-        if (this.isSameDate(m, d, y, this.fromDate)) {
-          classes.push('s-datepicker__date--from');
-        }
-        if (this.isSameDate(m, d, y, this.toDate)) {
-          classes.push('s-datepicker__date--to');
-        }
-      }
-      if (this.isSaturday(m, d, y)) {
-        classes.push('s-datepicker__date--saturday');
-      }
-      if (this.isSunday(m, d, y)) {
-        classes.push('s-datepicker__date--sunday');
-      }
-      if (this.isSameDate(m, d, y, this.today)) {
-        classes.push('s-datepicker__date--today');
-      }
-
-      return classes;
+    getDateClasses(m: number, d: number, y: number) {
+      const hasRange = this.range && !!this.fromDate && !!this.toDate;
+      return this.$class('date', {
+        between: hasRange && this.isInPeriod(m, d, y, this.fromDate, this.toDate),
+        from: hasRange && this.isSameDate(m, d, y, this.fromDate),
+        to: hasRange && this.isSameDate(m, d, y, this.toDate),
+        saturday: this.isSaturday(m, d, y),
+        sunday: this.isSunday(m, d, y),
+        today: this.isSameDate(m, d, y, this.today),
+      });
     },
 
     translateMonthName(monthKey: number, year: number) {
@@ -141,21 +125,22 @@ export default Vue.extend({
 
     isSameDate(m: number, d: number, y: number, date: Moment | undefined): boolean {
       if (!date) return false;
-      let dateInMonth = moment([y, (m - 1), d]).format('YYYY-MM-DD');
-      let compareDate = moment(date).format('YYYY-MM-DD');
-      return moment(dateInMonth).isSame(compareDate);
+      let dateInMonth = moment([y, (m - 1), d]);
+      return dateInMonth.isSame(date, 'day');
     },
 
-    isInPeriod(m: number, d: number, y: number, fromDate: Moment, toDate: Moment): boolean {
+    isInPeriod(
+      m: number, d: number, y: number,
+      fromDate: Moment | undefined,
+      toDate: Moment | undefined,
+    ): boolean {
       if (!fromDate || !toDate) return false;
       if (!this.range) return false;
-      let dateInMonth = moment([y, (m - 1), d]).format('YYYY-MM-DD');
-      let compareDateFrom = moment(fromDate).format('YYYY-MM-DD');
-      let compareDateTo = moment(toDate).format('YYYY-MM-DD');
-      return moment(dateInMonth).isBetween(compareDateFrom, compareDateTo);
+      let dateInMonth = moment([y, (m - 1), d]);
+      return dateInMonth.isBetween(fromDate, toDate, 'day');
     },
 
-    mousedown(payload: IMomentPayload, event: any) {
+    onMouseDown(payload: IMomentPayload, event: any) {
       if (event.button > 0) return;
       this.$emit('mouse-drag-start', payload);
     },
