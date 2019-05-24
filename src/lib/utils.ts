1e359e414b823b1b493d3b95f54735b9ca798751
Robin Pedersen <robin@smartdok.com>
2019-05-24 15:39:17 +0200

    Add default argument to utility function: get

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,15 @@
-export const get = (obj: any, path: string | string[]): any => {
+export const get = (obj: any, path: string | string[], defaultValue?: any): any => {
   if (typeof path === 'string') {
-    return get(obj, path.split('.'));
+    return get(obj, path.split('.'), defaultValue);
   }
   if (!obj || !path.length) return obj;
   const [first, ...rest] = path;
-  return get(obj[first], rest);
+
+  if (Object.prototype.hasOwnProperty.call(obj, first)) {
+    return get(obj[first], rest, defaultValue);
+  } else {
+    return defaultValue;
+  }
 };
 
 export const joinKeyPath = (keyPath: string[]) => keyPath.join(':');
