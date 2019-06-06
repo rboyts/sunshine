e72033f0940c39ae4b18b2a24ee1a8f0b853557b
Tage Guddingsmo <tage@visma.com>
2019-06-06 09:42:36 +0200

    Changed from svg to font + some css edits

--- src/components/STable/STableNoResults.vue
+++ src/components/STable/STableNoResults.vue
@@ -1,11 +1,11 @@
 <template>
   <div :class="$class()">
-    <img src="../../icons/svg/icon-circle-background.svg">
-    <img
+    <!-- <img src="../../icons/svg/icon-circle-background.svg"> -->
+    <div :class="$class('circle')" />
+    <div
+      class="sunshine24-search"
       :class="$class('search')"
-      src="../../icons/svg/search.svg"
-      alt="Search icon"
-    >
+    />
     <div :class="$class('header')">
       {{ $t('s-table-no-result.no-result') }}
     </div>
